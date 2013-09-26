306ac40acffe9aae6063d1e30177c9dca9080e0b
Slaven Tomac <slaventomac@gmail.com>
2013-09-26 21:08:26 +0200

    Cleared project by using only dependencies to outer .dll stored in /lib folder.

--- 2dXnaPhysicsDemo/DebugView XNA/DebugViewXNA.cs
+++ 2dXnaPhysicsDemo/DebugView XNA/DebugViewXNA.cs
@@ -13,6 +13,8 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
+using FarseerPhysics;
+
 namespace FarseerPhysics.DebugViews
 {
     /// <summary>
